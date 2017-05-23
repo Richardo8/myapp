4f97dd98a45b1cd64f2c8e36cc38d462d7ae62ba
LS <manunited_22@sina.com>
2017-05-23 17:01:30 +0800

    修改 App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, PropTypes, cloneElement } from 'react';
+import Tabs from './Tabs';
+import TabPane from './TabPane';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+
+        this.handelChange = this.handleChange.bind(this);
+
+        this.state = {
+            activeIndex: 0,
+        }
+    }
+
+    handelChange(e){
+        this.setState({
+            activeIndex: parseInt(e.target.value, 10),
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className="operator">
+                    <span>切换Tab：</span>
+                    <select value={this.state.activeIndex} onchange={this.handelChange}>
+                        <option value="0">Tab1</option>
+                        <option value="1">Tab2</option>
+                        <option value="2">Tab3</option>
+                    </select>
+                </div>
+                <Tabs defaultActiveIndex={this.state.activeIndex} className="tabs-bar">
+                    <TabPane order="0" tab={'Tab1'}>第一个Tab中的内容</TabPane>
+                    <TabPane order="1" tab={'Tab2'}>第二个Tab中的内容</TabPane>
+                    <TabPane order="2" tab={'Tab3'}>第三个Tab中的内容</TabPane>
+                </Tabs>
+            </div>
+        );
+    }
 }
 
 export default App;
